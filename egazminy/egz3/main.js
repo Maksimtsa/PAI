57a4241801ae04e354d374c7f88347b8a947ecdb
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-05-27 14:05:37 +0200

    Add files via upload

---
+++ egazminy/egz3/main.js
@@ -0,0 +1,13 @@
+const btn1 = document.querySelectorAll("btn1");
+const btn2 = document.querySelectorAll("btn2");
+
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+const p3 = document.getElementById("p3");
+const p4 = document.getElementById("p4");
+
+for (let i = 0; i <= btn1.length; i++) {
+    btn1[i].addEventListener("click", function () {
+
+    });
+}
