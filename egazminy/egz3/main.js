92636f46d83a62568cba665d0ae0350017fcf6ba
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-05-29 16:52:40 +0200

    Update main.js

--- egazminy/egz3/main.js
+++ egazminy/egz3/main.js
@@ -1,13 +1,37 @@
-const btn1 = document.querySelectorAll("btn1");
-const btn2 = document.querySelectorAll("btn2");
+const btn1 = document.querySelectorAll(".btn1");
+const btn2 = document.querySelectorAll(".btn2");
+const nameStuff = document.querySelectorAll(".name")
+const check = document.querySelectorAll(".check");
 
-const p1 = document.getElementById("p1");
-const p2 = document.getElementById("p2");
-const p3 = document.getElementById("p3");
-const p4 = document.getElementById("p4");
+let orderId = 0;
 
-for (let i = 0; i <= btn1.length; i++) {
-    btn1[i].addEventListener("click", function () {
+colorCheck();
 
+function colorCheck() {
+    check.forEach((element) => {
+        if (element.textContent == 0) {
+            element.style.backgroundColor = "Red";
+        }
+        else if (element.textContent >= 1 && element.textContent <= 5) {
+            element.style.backgroundColor = "Yellow";
+        }
+        else {
+            element.style.backgroundColor = "Honeydew";
+        }
     });
 }
+
+btn1.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+        const newValue = window.prompt("Podaj nową ilość: ");
+        check[index].textContent = newValue;
+        colorCheck();
+    });
+});
+
+btn2.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+        orderId++;
+        window.alert(`Zamówienie nr: ${orderId} Produkt: ${nameStuff[index].textContent}`);
+    });
+});
